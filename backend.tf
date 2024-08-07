00c2d8b273fe4ab562e3b671602a841176f6b63d
ricoclesca <ricardo.clesca@icloud.com>
2024-08-07 17:44:31 -0400

    week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "utc-app-pj7" #replace with your bucket
+    bucket         = "kdjarijij" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "fafdegfg" #replace with the table
   }
 }
